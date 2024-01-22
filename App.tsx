86cf05a3bf60b30fc2304d74b61f2cd9ecef6a39
ginnguyen123 <nhuunghia95@gmail.com>
2024-01-22 21:55:03 +0700

    update splach, asyc-story

--- App.tsx
+++ App.tsx
@@ -3,10 +3,16 @@ import { SplashScreen } from "./src/screens";
 import AuthNavigator from "./src/navigators/AuthNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native";
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
+import MainNavigator from "./src/navigators/MainNavigator";
 
 const App = () => {
   // App la noi dieu huong cac man hinh
   const  [isSplash, setIsSplash] = useState(true)
+  const [assetToken, setAssetToken] = useState('')
+
+  // cu phap getter, setter lay JWT
+  const {getItem, setItem} = useAsyncStorage('assetToken')
 
   useEffect(()=>{
     // setTimeout laf 1 callback function
@@ -14,8 +20,15 @@ const App = () => {
       setIsSplash(false)
     }, 1500)
 
+    checkLogin()
+
     return () => clearTimeout(timeout)
   },[])
+
+  const checkLogin = async () =>{
+    const token = await getItem()
+    token && setAssetToken(token)
+  }
 
   return <>
   {/* translucent => hinh splash la hinh nen cua StatusBar => tran man hinh*/}
@@ -29,7 +42,9 @@ const App = () => {
         <SplashScreen />
       ) : (
         <NavigationContainer>
-          <AuthNavigator />
+          {
+            assetToken ? <MainNavigator /> : <AuthNavigator />
+          }
         </NavigationContainer>
       )
     }
